fb12eaae4245499219509cde392752083ab76776
I'm Frieza Oh Oh Oh <31746868+FranciscoSimaoPerez@users.noreply.github.com>
2018-04-06 21:51:58 +0100

    ficha 21 nuxt

---
+++ fichas/index.vue
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <title>Exercício 7</title>
+</head>
+<body>
+    <script src="https://npmcdn.com/vue/dist/vue.js"></script> 
+    <div id ="exercicio7">
+        <button @click="toggle = !toggle">botão</button>
+    </div> 
+    <script>
+        new Vue({
+            el: '#exercicio7', 
+            data: {} 
+        }); 
+    </script>
+</body>
+</html
