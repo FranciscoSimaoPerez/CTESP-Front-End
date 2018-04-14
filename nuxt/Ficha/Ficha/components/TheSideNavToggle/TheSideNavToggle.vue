b99e606c10e3ca8b862ae5fb12a5d80aad584f0a
I'm Frieza Oh Oh Oh <31746868+FranciscoSimaoPerez@users.noreply.github.com>
2018-04-14 22:34:11 +0100

    ficha21 nuxt

---
+++ nuxt/Ficha/Ficha/components/TheSideNavToggle/TheSideNavToggle.vue
@@ -0,0 +1,7 @@
+<<template>
+    <div class="drawer-toggle" role="button">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+</template>
