dada1e84b169372ed5df97b26bc2d754614b5953
I'm Frieza Oh Oh Oh <31746868+FranciscoSimaoPerez@users.noreply.github.com>
2018-05-03 18:41:38 +0100

    ficha

---
+++ nuxt/Ficha/Ficha/pages/admin.vue
@@ -0,0 +1,60 @@
+<template>
+    <form @submit.prevent="submeterFormulario" class="form"> 
+        <div class="input-control">
+            <label for="">Produto</label>
+            <input type="text" v-model="novoProduto.produto">
+        </div>
+        <div class="input-control">
+            <label for="">Preço</label>
+            <input type="text" v-model="novoProduto.valor">
+        </div>
+        <button type="submit" class="button">Enviar</button>
+    </form>
+</template>
+
+<script>
+export default {
+    deta (){
+        return {
+            novoProduto:{
+                produto:'',
+                valor:''
+            }
+        }
+    
+    },
+    methods:{
+        submeterFormulario(){
+            //console.log(this.novoProduto)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .form{
+        max-width: 550px;
+        box-sizing: border-box;
+        margin: 30px;
+        margin-top: 60px;
+        padding: 30px;
+        text-align: justify;
+        box-shadow: 0 0 24px rgba(0, 0, 0, 0.3);
+        width: 100%;
+    }
+    .button {
+        font: inherit;
+        cursor: pointer;
+        border-radius: 4px;
+        border: 1px solid #06c4d1;
+        background-color: white;
+        color: #06c4d1;
+        text-decoration: none;
+        padding: 10px 30px; 
+    }
+    .button:hover,
+    .button:active {
+        color: #fff;
+        background-color: #06c4d1;
+    }
+</style>
